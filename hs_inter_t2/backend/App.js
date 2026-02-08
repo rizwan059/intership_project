403d7ca2e0b860cd448e5ee3929c0519a74fa4dd
Kabir-Sial <sial059689@gmail.com>
2026-02-08 21:41:31 +0500

    working on form handling with node js

--- hs_inter_t2/backend/App.js
+++ hs_inter_t2/backend/App.js
@@ -1,11 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const arg = process.argv;
-const port = arg[2];                        
+const port = arg[2];
 
 http.createServer((res, resp) => {
-    resp.setHeader('Content-Type', 'text/html');
-    resp.write("<h1>Hello from H&S Solutions!</h1>");
-    resp.end();
+
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            resp.writeHead(404, { 'Content-Type': 'text/html' });
+            resp.write("<h1>404 Not Found</h1>");
+            resp.end("internal function is running");
+            return;
+        }
+    });
+    resp.end("Hello from hs_inter_t2 backend!");
+
 
 }).listen(port);
